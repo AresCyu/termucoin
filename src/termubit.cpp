96c72c10b50a993c1e67d5264c00280b67455566
Sawano <aldianokto@gmail.com>
2026-06-22 12:33:01 +0700

    Update subsidy halving block count in termubit.cpp

--- src/termubit.cpp
+++ src/termubit.cpp
@@ -133,7 +133,7 @@ CAmount GetTermubitBlockSubsidy(int nHeight, const Consensus::Params& consensusP
         return 0;
 
     CAmount nSubsidy = 50 * COIN;
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
+    // Subsidy is cut in half every 1,280,000 blocks which will occur approximately every 7+ years.
     nSubsidy >>= halvings;
     return nSubsidy;
 }
